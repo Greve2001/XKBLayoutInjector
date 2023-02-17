cabf63619a294e13e0f3128364353a479a120222
Frederik Petersen <frederik.greve@gmail.com>
2023-02-17 22:28:11 +0100

    Made script take flags for different parameters. For now a name for the keyboard layout

--- injectLayout.sh
+++ injectLayout.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+while getopts 'n:' OPTION; do
+    case $OPTION in
+        n)
+            layout_name="$OPTARG"
+            echo "Layout Name: $layout_name"
+            ;;
+        ?)
+            echo "Wrong Usage"
+            ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
 SYMBOLS_PATH=/usr/share/X11/xkb/symbols
 RULES_PATH=/usr/share/X11/xkb/rules
 
-echo $SYMBOLS_PATH
+
