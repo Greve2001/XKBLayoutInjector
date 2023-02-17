565a66feb7bf9c5a05cf743969287c8942322b08
Frederik Petersen <frederik.greve@gmail.com>
2023-02-17 23:10:26 +0100

    Made argument check, so that all arguments must be parsed

--- injectLayout.sh
+++ injectLayout.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-while getopts 'n:' OPTION; do
+# Get arguments
+COUNTER=0
+while getopts n:a: OPTION; do
+    args+=("$OPTARG")
     case $OPTION in
-        n)
-            layout_name="$OPTARG"
-            echo "Layout Name: $layout_name"
+        n) # Layout Name
+            NAME="${args[$COUNTER]}"
+            let COUNTER++
+            ;;
+        a) # Layout Abbreviation
+            ABBR="${args[$COUNTER]}"
+            let COUNTER++
             ;;
         ?)
             echo "Wrong Usage"
+            exit 1
             ;;
     esac
 done
 shift "$(($OPTIND -1))"
+
+# Verify all arguments are given
+if [ -z "$NAME" ] || [ -z "$ABBR" ]; then
+    echo "Please provide all arguments"
+    exit 1
+fi
+
+echo "Layout Name: $NAME"
+echo "Layout Abbreviation: $ABBR"
 
 SYMBOLS_PATH=/usr/share/X11/xkb/symbols
 RULES_PATH=/usr/share/X11/xkb/rules
