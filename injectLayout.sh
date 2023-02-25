e9828c7c466f3611fe6a2ec719189579177eb76f
Frederik Petersen <frederik.greve@gmail.com>
2023-02-25 21:02:07 +0100

    Made the input taking more stable. Now gives detailed descriptions of errors

--- injectLayout.sh
+++ injectLayout.sh
@@ -8,20 +8,43 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
-## Get variables
-read -e -p "Path-to-file: " LAYOUT
-read -p "Name of Layout: " NAME
-read -p "Layout Abbreviation: " ABBR
+## Get variables and make sure they are valid
+# Layout / Path
+while true 
+do
+    read -e -p "Path-to-file: " LAYOUT
+    if [ -f "$LAYOUT" ]; then 
+        break
+    fi
+    echo "Please enter an existing file!"
+done 
+
+# Name
+while true 
+    do
+    read -p "Name of Layout: " NAME
+    if [ ! -z "$NAME" ]; then 
+        break 
+    fi
+    echo "The name must not be empty!"
+done
+
+# Abbreviation
+while true 
+    do
+    read -p "Layout Abbreviation: " ABBR
+    if [ ! -z "$ABBR" ]; then 
+        break 
+    fi
+    echo "The abbrivation must not be empty!"
+done
+
+# Description
 read -p "Description: " DESC
 
-## Verify all arguments are given 
-if [ -z "$NAME" ] || [ -z "$ABBR" ] || [ -z "$LAYOUT" ] || [ -z "$DESC" ]; then
-    echo "Please make sure all arguments are not null and valid"
-    exit 1
-fi
 
 ## Create symlink 
-ln -s $LAYOUT "$SYMBOLS_PATH"/$LAYOUT
+ln -s $LAYOUT "$SYMBOLS_PATH"/$LAYOUT || exit 1
 
 ## Add XML to file
 EVDEV="$RULES_PATH/evdev.xml"
@@ -29,3 +52,7 @@ NEW_FILE="$(./xml_printer.awk -v NAME="$NAME" -v ABBR="$ABBR" -v LAYOUT="$LAYOUT
 
 ## Overwrite file
 echo "$NEW_FILE" > $EVDEV
+
+## Print success message
+echo "Injection Complete!"
+echo "Restart your machine to detect the layout."
