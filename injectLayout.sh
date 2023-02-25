0899f1db3651e2bb1f474e8eb625c3ba977dbaaf
Frederik Petersen <frederik.greve@gmail.com>
2023-02-25 22:07:59 +0100

    Tried rebuilding how i take inputs. Does not work for now

--- injectLayout.sh
+++ injectLayout.sh
@@ -1,4 +1,98 @@
 #!/bin/bash
+
+## Print help information
+function help_info {
+    echo "Help Comming!"
+}
+
+
+### Different Input Methods ###
+## Sequential Prompting
+function input_prompting {
+    # Layout / Path
+    while true 
+    do
+        read -e -p "Path-to-file: " LAYOUT
+        if [ -f "$LAYOUT" ]; then 
+            break
+        fi
+        echo "Please enter an existing file!"
+    done 
+
+    # Name
+    while true 
+        do
+        read -p "Name of Layout: " NAME
+        if [ ! -z "$NAME" ]; then 
+            break 
+        fi
+        echo "The name must not be empty!"
+    done
+
+    # Abbreviation
+    while true 
+        do
+        read -p "Layout Abbreviation: " ABBR
+        if [ ! -z "$ABBR" ]; then 
+            break 
+        fi
+        echo "The abbrivation must not be empty!"
+    done
+
+    # Description
+    read -p "Description: " DESC
+}
+
+## Oneliner with flags.
+function input_oneliner {
+    COUNTER=1
+    while getops f:n:a:d: OPTION; do
+        args+=("$OPTARG")
+        case $OPTION in
+            f) # Layout File Name
+                LAYOUT="${args[$COUNTER]}"
+                let COUNTER++
+                ;;
+            n) # Layout Name
+                NAME="${args[$COUNTER]}"
+                let COUNTER++
+                ;;
+            a) # Layout Abbreviation
+                ABBR="${args[$COUNTER]}"
+                let COUNTER++
+                ;;
+            d) # Description
+                DESC="${args[$COUNTER]}"
+                let COUNTER++
+                ;;
+            ?)
+                echo "Wrong Usage"
+                help_info
+                exit 1
+                ;;
+        esac
+    done
+    echo "OPTIND: $OPTIND"
+    shift "$(($OPTIND -1))"
+
+    echo "Layout: $LAYOUT"
+    echo "Name: $NAME"
+    echo "Abbr: $ABBR"
+    echo "Desc: $DESC"
+
+    ## Verify all arguments are given ##
+    if [ -z "$NAME" ] || [ -z "$ABBR" ] || [ ! -f "$LAYOUT" ]; then
+        echo "Please provide all arguments"
+        help_info
+        exit 1
+    fi
+}
+
+
+
+
+### ---------- Start of code ---------- ###
+
 SYMBOLS_PATH=/usr/share/X11/xkb/symbols
 RULES_PATH=/usr/share/X11/xkb/rules
 
@@ -8,40 +102,22 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
-## Get variables and make sure they are valid
-# Layout / Path
-while true 
-do
-    read -e -p "Path-to-file: " LAYOUT
-    if [ -f "$LAYOUT" ]; then 
-        break
-    fi
-    echo "Please enter an existing file!"
-done 
 
-# Name
-while true 
-    do
-    read -p "Name of Layout: " NAME
-    if [ ! -z "$NAME" ]; then 
-        break 
-    fi
-    echo "The name must not be empty!"
-done
+## Find out what first argument is and act accordingly
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    help_info
+    exit 0
 
-# Abbreviation
-while true 
-    do
-    read -p "Layout Abbreviation: " ABBR
-    if [ ! -z "$ABBR" ]; then 
-        break 
-    fi
-    echo "The abbrivation must not be empty!"
-done
+elif [ "$1" == "-q" ] || [ "$1" == "--quick" ]; then
+    input_oneliner
 
-# Description
-read -p "Description: " DESC
+elif [ -z "$1" ]; then
+    input_prompting
 
+else
+    help_info
+    exit 1
+fi
 
 ## Create symlink 
 ln -s $LAYOUT "$SYMBOLS_PATH"/$LAYOUT || exit 1
@@ -56,3 +132,4 @@ echo "$NEW_FILE" > $EVDEV
 ## Print success message
 echo "Injection Complete!"
 echo "Restart your machine to detect the layout."
+exit 0
